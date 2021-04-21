1030ce8c8e155a710b576413c6a3a80f0ad0819b
smritiwadhwa <78246269+smritiwadhwa@users.noreply.github.com>
2021-04-20 21:01:51 -0700

    Add files via upload

---
+++ MySolution.java
@@ -0,0 +1,44 @@
+package oddeven;
+import java.io.*;
+import java.io.File;  // importing the File class
+import java.io.FileNotFoundException;  // importing this class for errors
+import java.util.Scanner; // importing the Scanner class to read text files
+public class MySolution {
+public static void main(String[] args) {
+	  FileWriter outputStreamEven = null;
+	  FileWriter outputStreamOdd = null;
+	  System.out.println("Command Line: " + args.length);
+	try {
+		outputStreamEven = new FileWriter("even.txt");
+		outputStreamOdd = new FileWriter("odd.txt");
+            File myObj = new File("in_2.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              int number = Integer.parseInt(data);
+              if (number % 2 == 0) {
+                  outputStreamEven.write(number + "\n");
+              } else {
+                  outputStreamOdd.write(number + "\n");
+              }
+            }
+            myReader.close();
+            if (outputStreamEven != null) {
+                outputStreamEven.close();
+            }
+            if (outputStreamOdd != null) {
+                outputStreamOdd.close();
+            }
+          } catch (FileNotFoundException e) {
+            System.out.println("A FileNotFound error occurred.");
+            e.printStackTrace();
+          } catch (IOException e) {
+        	  System.out.println("An IOException error occured.");
+        	  e.printStackTrace();
+        	  
+          } 
+
+}
+
+
+}
